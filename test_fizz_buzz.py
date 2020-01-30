4c4fba7cf9350976451ce04feb68a0cc7146220e
Josh Torbett <josh.torbett@getfutureproof.co.uk>
2020-01-30 19:49:56 +0000

    add test to fizz buzz if multiple of 3 & 5

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -13,6 +13,11 @@ class Test_fizz_buzz(unittest.TestCase):
     def test_buzz(self):
         self.assertEqual(fizz_buzz.fizz_buzz(5),'Buzz')
         self.assertEqual(fizz_buzz.fizz_buzz(65),'Buzz')
+    #returns FizzBuzz on multiples of 3 & 5
+    def test_buzz(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(15),'FizzBuzz')
+        self.assertEqual(fizz_buzz.fizz_buzz(90),'FizzBuzz')
+
 
     
     
