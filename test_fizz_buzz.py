a73c7499c023ff1e7d1b11f197fba8991d23ef53
Josh Torbett <josh.torbett@getfutureproof.co.uk>
2020-01-30 19:37:59 +0000

    add test to fizz if multiple of 3

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -5,6 +5,12 @@ class Test_fizz_buzz(unittest.TestCase):
     #returns a number
     def test_number(self):
         self.assertEqual(fizz_buzz.fizz_buzz(1),1)
+    #returns fizz on multiples of 3
+    def test_fizz(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(3),'Fizz')
+        self.assertEqual(fizz_buzz.fizz_buzz(42),'Fizz')
+    
+    
 
 #to allow to run tests in terminal with 'python test_fizz_buzz.py'
 if __name__ == '__main__':
